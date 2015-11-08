61eb067c91f1090a00feccf1ba275da33275c786
chrislill <chris_lill_uk@hotmail.com>
2015-11-08 05:34:54 +0000

    Minor wording change

--- ui.R
+++ ui.R
@@ -10,9 +10,9 @@ shinyUI(fluidPage(
     checkboxInput("rounding", "Round payments?")
   ),
   mainPanel(
-    p('This page lets you distribute costs between people, based on a number of 
-      shares. This enables a fairer way of sharing between families and individuals.'),
-    p("The default values give an example for sharing costs between my friends' families."),
+    p("This page lets you distribute costs between people, based on a number of 
+      shares. This enables a fairer way of sharing between families and individuals. 
+      The default values give an example for sharing costs between my friends' families."),
     p("Set the cost, specify how many shares per person, and then copy the results into an email."),
     p('Please see the cost sharing presentation for more details.'),
     # verbatimTextOutput("debug"),
