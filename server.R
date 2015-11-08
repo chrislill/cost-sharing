45fed563efc7100d8673d11cc3f86379ec1602a2
chrislill <chris_lill_uk@hotmail.com>
2015-11-08 04:16:46 +0000

    Implemented reset names and rounding

--- server.R
+++ server.R
@@ -1,26 +1,50 @@
 library(shiny)
 library(rhandsontable)
 
-# The Nottingham group are a set of friends from university
-# We'll use them as a pre-populated data set
-# nottingham.group 
-cost.table <- data.frame(name = c('Ashby', 'Bradley', 'Grogan', 'Judge', 'Laud', 
-                                      'Lill', 'Megan', 'Pollard', 'Quinlan'),
-                            shares = as.integer(c(3, 3, 2, 3, 3, 3, 3, 3, 1)),
-                            payment = 0,
-                            stringsAsFactors = FALSE)
-
 shinyServer(
   function(input, output) {
-    output$debug <- renderPrint({sum(cost.table$shares)})
+    # Set initial values
+    retrieve.table <- TRUE    
+    
+    # The Nottingham group are a set of friends from university
+    # We'll use them as a pre-populated data set for each new user
+    cost.table <- data.frame(name = c('Ashby', 'Bradley', 'Grogan', 'Judge', 'Laud', 
+                                      'Lill', 'Megan', 'Pollard', 'Quinlan'),
+                             shares = as.integer(c(3, 3, 2, 3, 3, 3, 3, 3, 1)),
+                             payment = 0,
+                             stringsAsFactors = FALSE)
+
+    
+    # ActionButton for Reset Names 
+    observeEvent(input$reset, {
+        cost.table <<- data.frame(name = "",
+                                 shares = as.integer(c(1, 1)),
+                                 payment = 0, 
+                                 stringsAsFactors = FALSE)
+        retrieve.table <<- FALSE
+    },
+    priority = 1)
+
+    output$debug <- renderPrint(retrieve.table)
 
     output$table <- renderRHandsontable({
-      if (!is.null(input$table)) {
+      if (input$reset > 0) {}
+      
+      if (!is.null(input$table) && retrieve.table == TRUE) {
         cost.table <- hot_to_r(input$table)
         names(cost.table) <- c("name", "shares", "payment")
-        } 
+      } 
+      retrieve.table <<- TRUE
+      
+      if(input$rounding == TRUE) {
+        payment.digits = 0
+      } else {
+        payment.digits = 2
+      }
+      
       share.cost <- as.numeric(input$cost) / sum(cost.table$shares)
-      cost.table$payment <- share.cost * cost.table$shares
+      cost.table$payment <- round(share.cost * cost.table$shares, payment.digits)
+      
       rhandsontable(cost.table, rowHeaders = NULL, colHeaders = c("Names", 
                                                                   "Shares", 
                                                                   "Payment")
--- ui.R
+++ ui.R
@@ -3,12 +3,17 @@ library(rhandsontable)
 shinyUI(fluidPage(
   headerPanel("Cost sharing calculator"),
   sidebarPanel(
-    textInput("cost", "Cost", value = "0")
+    textInput("cost", "Cost", value = "0"),
+    actionButton("reset", "Reset Names"),
+    p(),
+    actionButton("addRow", "Add Row"),
+    checkboxInput("rounding", "Round payments?")
   ),
   mainPanel(
     p('This page lets you distribute costs between people, based on a number of 
       shares. This enables a fairer way of sharing between families and individuals.'),
-    p('Please see the cost sharing introduction for more details.'),
+    p("The default values give an example for sharing costs between my friends' families."),
+    p('Please see the cost sharing presentation for more details.'),
     verbatimTextOutput("debug"),
     rHandsontableOutput('table')
     )
